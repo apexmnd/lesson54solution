5bd87d6a4f099d0709f85f739eb5f8a298e4f041
Anton Aliaksandrau <antonalex2021@gmail.com>
2026-05-15 18:39:34 +0300

    upd

---
+++ task01project/test03.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//
+//void test(int count) {
+//	cout << "count = " << count << endl;
+//	test(++count);
+//}
+//
+//int main() {
+//	test(1);
+//
+//	return 0;
+//}
