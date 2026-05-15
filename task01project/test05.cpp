430806f37d728d201591fa93a4845fd081d9da01
Anton Aliaksandrau <antonalex2021@gmail.com>
2026-05-15 19:43:33 +0300

    upd

--- task01project/test05.cpp
+++ task01project/test05.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-using namespace std;
-
-long long factorial(int num) {
-	// base-case	
-	if (num == 0 or num == 1) {
-		return 1;
-	}
-	// recursion-case
-
-
-	return factorial(--num) * num;
-}
-
-int main() {
-	int num;
-
-	cout << "Input your positive number: ";
-	cin >> num;
-
-	long long result = factorial(num);
-
-	cout << num << "! = " << result << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//long long factorial(int num) {
+//	
+//	// base-case	
+//	if (num == 0 or num == 1) {
+//		return 1;
+//	}
+//	
+//	// recursion-case
+//	return factorial(num - 1) * num;
+//}
+//
+//int main() {
+//	int num;
+//
+//	cout << "Input your positive number: ";
+//	cin >> num;
+//
+//	long long result = factorial(num);
+//
+//	cout << num << "! = " << result << endl;
+//
+//	return 0;
+//}
